7a2e6357833a53d18b8986dd5dc5cbc2fba154e0
Jckf <jckf@jckf.no>
2012-09-14 05:17:57 +0200

    Implemented operation execution time emulation.

--- src/no/jckf/gameboy/CPU.java
+++ src/no/jckf/gameboy/CPU.java
@@ -16,6 +16,7 @@ public class CPU {
 
 	// System variables.
 	private boolean halted = false;
+	private int cycles = 0;
 
 	// Registers.
 	private int a = 1;
@@ -81,6 +82,11 @@ public class CPU {
 	public boolean cycle() {
 		if (halted) return false;
 
+		if (cycles != 0) {
+			cycles--;
+			return true;
+		}
+
 		int opcode = mmu.readByte(pc++) & 0xFF;
 
 		println(hexw(pc - 1) + " " + hexb(opcode) + " " +
@@ -122,17 +128,21 @@ public class CPU {
 
 	// NOP = No operation.
 	private void _0x00() {
-
+		cycles += 4;
 	}
 
 	// LD bc d16 = Copy the following 2 bytes into the b and c registers.
 	private void _0x01() {
+		cycles += 12;
+
 		c = mmu.readByte(pc++);
 		b = mmu.readByte(pc++);
 	}
 
 	// DEC bc = Decrement 16 bit bc register.
 	private void _0x0B() {
+		cycles += 8;
+
 		int _bc = bc() - 1;
 		bc(_bc);
 
@@ -145,65 +155,105 @@ public class CPU {
 
 	// JR r8 = Jump relative to current PC as defined by the following byte.
 	private void _0x18() {
+		cycles += 12;
+
 		pc += mmu.readByte(pc) + 1;
 	}
 
 	// JR nz r8 = Jump relative to current PC as defined by the following byte if zero flag isn't set.
 	private void _0x20() {
-		if (!z()) pc += mmu.readByte(pc);
+		cycles += 8;
+
+		if (!z()) {
+			cycles += 4;
+			pc += mmu.readByte(pc);
+		}
+
 		pc++;
 	}
 
 	// LD hl d16 = Copy the following two bytes (word) into high and low registers (16 bit hl register).
 	private void _0x21() {
+		cycles += 12;
+
 		l = mmu.readByte(pc++);
 		h = mmu.readByte(pc++);
 	}
 
 	// INC hl = Increment the 16 bit hl register.
 	private void _0x23() {
+		cycles += 8;
+
 		hl(hl() + 1);
 	}
 
 	// JR z r8 = Jump relative to current PC as defined by next byte if zero flag is set.
 	private void _0x28() {
-		if (z()) pc += mmu.readByte(pc);
+		cycles += 8;
+
+		if (z()) {
+			cycles += 4;
+			pc += mmu.readByte(pc);
+		}
+
 		pc++;
 	}
 
 	// LD sp d16 = Copy the following word into the stack pointer.
 	private void _0x31() {
+		cycles += 12;
+
 		sp = mmu.readWord(pc);
 		pc += 2;
 	}
 
 	// LD (hl) d8 = Copy the following byte into the address defined by the high and low registers.
 	private void _0x36() {
+		cycles += 12;
+
 		mmu.writeByte(hl(),mmu.readByte(pc++));
 	}
 
 	// LD a d8 = Copy the following byte into the accumulator.
 	private void _0x3E() {
+		cycles += 8;
+
 		a = mmu.readByte(pc++);
 	}
 
 	// LD b a = Copy the accumulator's value into the b register.
 	private void _0x47() {
+		cycles += 4;
+
 		b = a;
 	}
 
 	// HALT = Stop execution.
 	private void _0x76() {
+		cycles += 4;
+
 		halted = true;
 	}
 
 	// LD a b = Copy register b's value into the accumulator.
 	private void _0x78() {
+		cycles += 4;
+
 		a = b;
+	}
+
+	// XOR a = "Exclusive or" accumulator against itself, effectively zeroing it out.
+	private void _0xAF() {
+		cycles = 4;
+
+		a = 0;
+		z(true);
 	}
 
 	// OR c = "Or" register c against the accumulator.
 	private void _0xB1() {
+		cycles += 4;
+
 		a |= c;
 
 		if (a == 0) {
@@ -215,11 +265,15 @@ public class CPU {
 
 	// JP a16 = Jump to absolute address defined in the following word.
 	private void _0xC3() {
+		cycles += 16;
+
 		pc = mmu.readWord(pc);
 	}
 
 	// 0xCB prefixed opcodes.
 	private void _0xCB() {
+		cycles += 4;
+
 		int opcode = mmu.readByte(pc++) & 0xFF;
 
 		println(hexw(pc - 1) + " 0xCB " + hexb(opcode));
@@ -232,11 +286,16 @@ public class CPU {
 
 	// RES 0 a = Reset bit 0 of accumulator.
 	private void _0xCB87() {
+		// Is it 8 including the 4 from 0xCB?
+		cycles += 8;
+
 		a &= 0xFE;
 	}
 
 	// CALL a16 = Move down the stack and jump to address defined in the following word.
 	private void _0xCD() {
+		cycles += 24;
+
 		sp -= 2; // Move back to make room for PC.
 		mmu.writeWord(sp,pc + 2); // Copy old PC to stack.
 		pc = mmu.readWord(pc); // Jump to wherever.
@@ -244,35 +303,37 @@ public class CPU {
 
 	// LDH (a8) a = Copy accumulator value to IO port defined in the following byte.
 	private void _0xE0() {
+		cycles += 12;
+
 		mmu.writeByte(0xFF00 + mmu.readByte(pc++),a);
 	}
 
 	// LD a16 a = Copy accumulator value into address defined in the following word.
 	private void _0xEA() {
+		cycles = 16;
+
 		mmu.writeByte(mmu.readWord(pc),a);
 		pc += 2;
 	}
 
 	// LDH a (a8) = Copy from given IO port into accumulator.
 	private void _0xF0() {
+		cycles += 12;
+
 		a = mmu.readByte(0xFF00 + mmu.readByte(pc++));
 	}
 
 	// DI = Disable interrupts.
 	private void _0xF3() {
-
+		cycles += 4;
 	}
 
 	// CP d8 = Compare the following byte to accumulator.
 	private void _0xFE() {
+		cycles += 8;
+
 		int b = mmu.readByte(pc++) & 0xFF;
 		if (b == a) { z(true); } else { z(false); }
 		n(true);
-	}
-
-	// XOR a = "Exclusive or" accumulator against itself, effectively zeroing it out.
-	private void _0xAF() {
-		a = 0;
-		z(true);
 	}
 }
